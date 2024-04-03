2fea4f016278b79c5eb80b73afe334025693c9b0
hannakulynych <160737448+hannakulynych@users.noreply.github.com>
2024-04-03 13:24:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alledrogo</title>
+    <link rel="stylesheet" href="styless.css">
+</head>
+<body>
+    <header class="flex-container topnav">
+        <img src="img/image3.jpeg" alt="logo" id="logo">
+        <form action="search.php" method="get">
+        <input type="text" name="search" id="search" placeholder="Wyszukaj produkt">
+        <input type="submit" name="Szukaj">
+        <a href="cart.php" id="cart">Koszyk</a>
+    </header>
+    <main class="flex-container-wrap">
+     <div class="card">
+        <h3>nazwa oferty</h3>
+        <p>Cena: 100pln</p>
+        </div>
+     <div class="card">
+        </div>
+        <div class="card">
+        </div>
+</body>
+</html>
